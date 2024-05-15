fb47045a19e79fe8259fc7d65aada8303f372a7b
noStackEngineer <47726411+noStackEngineer@users.noreply.github.com>
2024-05-15 15:01:15 -0700

    command-line model selection and smoothing

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 from models import *
+import argparse
 
 def main():
-    traindata = open('1b_benchmark.train.tokens', 'r', encoding='utf8')
-    testdata = open("1b_benchmark.dev.tokens", "r", encoding='utf8')
+    # Get command-line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', type=str, default='Unigram',
+                        choices=['Unigram', 'Bigram', 'Trigram'])
+    parser.add_argument('--smoothing', '-s', type=int, default=0)
+    args = parser.parse_args()
+
+    # Ensure smoothing argument is non-negative
+    if args.smoothing < 0:
+        print("Negative number passed to --smoothing; defaulting to 0")
+        args.smoothing = 0
+
+    # Load data
+    with open('1b_benchmark.train.tokens', 'r', encoding='utf8') as train_file:
+        train_data = train_file.read()
+    with open("1b_benchmark.dev.tokens", 'r', encoding='utf8') as test_file:
+        test_data = test_file.read()
+
+    if args.model == "Unigram":
+        model = UnigramModel(args.smoothing)
+    elif args.model == "Bigram":
+        model = BigramModel(args.smoothing)
+    elif args.model == "Trigram":
+        model = TrigramModel(args.smoothing)
+    else:
+        raise Exception("Pass Unigram, Bigram, or Trigram to --model")
+
+    model.train(train_data)
+    model.perplexity(test_data)
+
     '''model = UnigramModel()
     model.train(traindata)
-    model.smoothperplexity(testdata, 1)'''
+    model.smoothperplexity(testdata, 1)
     model = BigramModel()
-    model.train(traindata)
-    model.perplexity(testdata)
+    model.train(train_data)
+    model.perplexity(test_data)
+    '''
 
 if __name__ == '__main__':
     main()
