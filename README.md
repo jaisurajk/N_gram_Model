c709ab0716b1c4627f37bdf62eced7439137aa7f
Liam Xu <liamusxu@gmail.com>
2024-05-21 23:48:40 -0700

    got trigrams working for the debug set, fixed bigram

--- README.md
+++ README.md
@@ -2,4 +2,8 @@ Run the model with python main.py -m [Unigram/Bigram/Trigram] depending on which
 
 Run with smoothing: python main.py -m [Unigram/Bigram/Trigram] -s [smoothing value]
 
+<<<<<<< HEAD
 Run trigram model with linear interpolation smoothing: python main.py -m Trigram -i 1
+=======
+Run trigram model with linear interpolation smoothing: python main.py -m Trigram -i 1
+>>>>>>> bd733bd (linear interpolation)
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import argparse
 def main():
     # Get command-line args
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', type=str, default='Unigram',
-                        choices=['Unigram', 'Bigram', 'Trigram'])
+    parser.add_argument('--model', '-m', type=str, default='Unigram', choices=['Unigram', 'Bigram', 'Trigram'])
     parser.add_argument('--smoothing', '-s', type=float, default=0)
+    parser.add_argument('--interpolate', '-i', type=int, default=0)
     args = parser.parse_args()
 
     # Ensure smoothing argument is non-negative
@@ -20,7 +20,7 @@ def main():
     elif args.model == "Bigram":
         model = BigramModel(args.smoothing)
     elif args.model == "Trigram":
-        model = TrigramModel(args.smoothing)
+        model = TrigramModel(args.smoothing, args.interpolate)
     else:
         raise Exception("Pass Unigram, Bigram, or Trigram to --model")
     
@@ -32,12 +32,12 @@ def main():
             train_file.seek(0)
             print("train perplexity: " + str(model.perplexity(train_file)))
     
-    # load dev set and calculate perplexity
     with open("data/1b_benchmark.dev.tokens", 'r', encoding='utf8') as dev_file:
             print("dev perplexity: " + str(model.perplexity(dev_file)))
 
+    with open("data/1b_benchmark.test.tokens", 'r', encoding='utf8') as test_file:
+            print("test perplexity: " + str(model.perplexity(test_file)))
 
-    # use debug set as frame of reference
     with open("data/debug.tokens", 'r', encoding='utf8') as debug_file:
             print("debug perplexity: " + str(model.perplexity(debug_file)))
 
